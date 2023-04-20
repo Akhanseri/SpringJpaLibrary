9616b7c92a97620f77e9fcc3ea441d20e0941ab2
Ali <114877417+Akhanseri@users.noreply.github.com>
2023-04-20 11:45:53 +0200

    Library Project

---
+++ src/main/java/kz/ali/repositories/BooksRepository.java
@@ -0,0 +1,12 @@
+package kz.ali.repositories;
+
+import kz.ali.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
+}
